2eeba33c3941ca409e7bdef046578ce987f708ba
Leonardo <leonoman07@gmail.com>
2022-08-25 15:04:46 -0300

    update record lmao

--- src/components/Body/body.jsx
+++ src/components/Body/body.jsx
@@ -4,7 +4,7 @@ import Fumar2 from "../../img/fumar2.jpeg";
 
 const Body = () => {
   const getDiasSemFumar = () => {
-    const ultimoDia = new Date(2022, 7, 20, 22, 3, 0);
+    const ultimoDia = new Date(2022, 7, 24, 10, 3, 0);
     const hoje = new Date();
     const diasSemFumar = hoje.getTime() - ultimoDia.getTime();
     const dias = new Date(diasSemFumar).getDate();
@@ -12,7 +12,7 @@ const Body = () => {
   };
 
   const getRecorde = () => {
-    const recorde = 0;
+    const recorde = 3;
     const diasSemFumar = getDiasSemFumar();
     if (diasSemFumar > recorde) {
       return diasSemFumar;
